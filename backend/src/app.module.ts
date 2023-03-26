d42f312b9c5f2894ac17f8b902da26276dc58c4e
lafin716 <lafin716@gmail.com>
2023-03-26 20:57:59 +0900

    feat: nest config 설치 및 .env 설정

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { LoginModule } from './login/login.module';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [LoginModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NDOE_ENV === 'dev' ? '.env.dev' : '.env.prod',
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [],
 })
