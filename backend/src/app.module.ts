b6547aa2e777168fc6168865ad8746f7b79618ba
lafin716 <lafin716@gmail.com>
2023-03-26 21:17:54 +0900

    fix: mongo 설정 단일 URI로 변경

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { UserModule } from './user/user.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: process.env.NDOE_ENV === 'dev' ? '.env.dev' : '.env.prod',
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get('MONGO_URI'),
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        inject: [ConfigService],
+      }),
     }),
     UserModule,
   ],
