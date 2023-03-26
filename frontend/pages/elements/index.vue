8a3f7064bc57ebfbb40f0f9300ee312dd3341bcc
lafin716 <lafin716@gmail.com>
2023-03-26 18:31:57 +0900

    feat: 기본 테이블 디자인 작업

--- frontend/pages/elements/index.vue
+++ frontend/pages/elements/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import ContentCard from "@/components/layout/part/ContentCard.vue";
 import { ref } from "vue";
+import { PencilIcon, TrashIcon } from "vue-tabler-icons";
 
 const headers = ref([
   { text: "라벨", value: "name" },
@@ -11,6 +12,7 @@ const headers = ref([
 
 const items = ref([
   {
+    id: 1,
     name: "이름입력",
     type: "텍스트",
     created_at: "2021-08-01",
@@ -24,7 +26,49 @@ const items = ref([
       <v-btn class="bg-primary">새로 만들기</v-btn>
     </template>
     <template v-slot:contents>
-      <v-data-table :headers="headers" :items="items"></v-data-table>
+      <v-table class="month-table">
+        <thead>
+          <tr>
+            <th class="text-subtitle-1 font-weight-bold text-center">번호</th>
+            <th class="text-subtitle-1 font-weight-bold text-center">라벨</th>
+            <th class="text-subtitle-1 font-weight-bold text-center">타입</th>
+            <th class="text-subtitle-1 font-weight-bold text-center">
+              등록날짜
+            </th>
+            <th class="text-subtitle-1 font-weight-bold text-center">관리</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in items" :key="item.name" class="month-item">
+            <td class="text-center">
+              {{ item.id }}
+            </td>
+            <td class="text-center">
+              {{ item.name }}
+            </td>
+            <td class="text-center">
+              <v-chip
+                :class="'text-body-1 bg-success'"
+                color="white"
+                size="small"
+              >
+                {{ item.type }}
+              </v-chip>
+            </td>
+            <td class="text-center">
+              <h6 class="text-body-1 text-muted">{{ item.created_at }}</h6>
+            </td>
+            <td class="text-center">
+              <v-btn icon variant="flat">
+                <PencilIcon class="text-info" />
+              </v-btn>
+              <v-btn icon variant="flat">
+                <TrashIcon class="text-error" />
+              </v-btn>
+            </td>
+          </tr>
+        </tbody>
+      </v-table>
     </template>
   </ContentCard>
 </template>
--- frontend/stores/elementStore.ts
+++ frontend/stores/elementStore.ts
@@ -3,7 +3,7 @@ import { ElementTypeData } from "~~/components/element/elementData";
 import { ElementType } from "~~/types/fastform/Element";
 
 export const useElementStore = defineStore("element", () => {
-  const type: ElementType = ElementTypeData.FILE;
+  const type: ElementType = ElementTypeData.TEXT;
   const label: string = "항목 1";
   const data: any = {};
 
