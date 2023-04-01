cfb0153216a6b715b7c36b079f9acb8cb5eea9f5
lafin716@gmail.com <lafin716@gmail.com>
2023-04-01 18:27:16 +0900

    feat: 엘리먼트 api 수정

--- backend/src/element/element.controller.ts
+++ backend/src/element/element.controller.ts
@@ -1,38 +1,17 @@
-import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { ElementService } from './element.service';
-import { JwtAuthGuard } from 'src/auth/jwt/jwt.guard';
-import { CreateElementDto } from './dto/create-element.dto';
 
 @Controller('element')
-@UseGuards(JwtAuthGuard)
+UseGuards(JwtAuthGuard)
 export class ElementController {
   constructor(private readonly elementService: ElementService) {}
 
   @Get()
-  getAllElements(@Req() req) {
-    const userId = req.user.iss;
+  getAllElements(@Req() req) { 
+    const userId = req.user.id;
     return this.elementService.getAllElements(userId);
   }
-
-  @Get(':id')
-  getElement(@Req() req) {
-    const userId = req.user.iss;
-    const elementId = req.params.id;
-    return this.elementService.getElement(userId, elementId);
-  }
-
+  
   @Post()
-  createElement(@Body() body: CreateElementDto, @Req() req) {
-    console.log(req.user);
-
-    const userId = req.user.iss;
-    return this.elementService.createElement(userId, body);
-  }
-
-  @Post(':id')
-  updateElement(@Body() body: CreateElementDto, @Req() req) {
-    const userId = req.user.iss;
-    const elementId = req.params.id;
-    return this.elementService.updateElement(userId, elementId, body);
-  }
+  createElement();
 }
