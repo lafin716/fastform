72437fe4d1056d96ef752c8c9ef430af8148f52d
lafin716 <lafin716@gmail.com>
2023-04-06 22:18:01 +0900

    feat: 토스트 기본 옵션 수정

--- frontend/plugins/vue-toastify.client.ts
+++ frontend/plugins/vue-toastify.client.ts
@@ -3,7 +3,18 @@ import * as vt from "vue-toastification";
 import "vue-toastification/dist/index.css";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(vt.default);
+  nuxtApp.vueApp.use(vt.default, {
+    timeout: 2500,
+    position: "top-right",
+    transition: "Vue-Toastification__bounce",
+    maxToasts: 20,
+    newestOnTop: true,
+    closeOnClick: true,
+    pauseOnFocusLoss: false,
+    pauseOnHover: false,
+    draggable: false,
+    hideProgressBar: true,
+  });
   return {
     provide: {
       toast: vt.useToast(),
