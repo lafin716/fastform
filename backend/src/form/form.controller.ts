754576e61631187d510550a0fadf4ce3e0361394
lafin716 <lafin716@gmail.com>
2023-04-09 15:23:54 +0900

    feat: form 모듈 시작

--- backend/src/form/form.controller.ts
+++ backend/src/form/form.controller.ts
@@ -1,8 +1,8 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('form')
 export class FormController {
-  
+  @Get()
   getForm() {
     console.log(1);
   }
